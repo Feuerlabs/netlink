a6efec25bf29c0e4b8c7c27651e224558e69b5a1
Tony Rogvall <tony@rogvall.se>
2013-08-26 14:29:46 +0200

    get_value api to read interface statistics

--- src/netlink_stat.erl
+++ src/netlink_stat.erl
@@ -11,7 +11,77 @@
 
 -include("../include/netlink.hrl").
 
-get_value(Interface) ->
+get_value() ->
+    get_value("*").
+
+get_value("") ->
+    get_value("*");
+get_value(Counter) ->
+    case string:tokens(Counter, ".") of
+	[]         -> get_all_counters();
+	["*"]      -> get_all_counters();
+	["*","*"]  -> get_all_counters();
+	[Name,"*"] -> get_all_counters(Name);
+	[Name]     -> get_all_counters(Name);
+	["*",Var] ->
+	    Field = list_to_atom(Var),
+	    Fi = index(Field, record_info(fields, rtnl_link_stats)),
+	    if Fi =:= 0 ->
+		    [];
+	       true ->
+		    {ok,Ifs} = inet:getiflist(),
+		    select_counters_(Ifs, Fi+1, Var, [])
+	    end;
+	[Name,Var] ->
+	    Field = list_to_atom(Var),
+	    {ok,S} = get_statistics(Name),
+	    case index(Field, record_info(fields, rtnl_link_stats)) of
+		0 -> [];
+		Fi -> [{Counter,element(Fi+1, S)}]
+	    end
+    end.
+
+get_all_counters() ->
+    {ok,Ifs} = inet:getiflist(),
+    get_all_counters_(Ifs,[]).
+
+get_all_counters(Name) ->
+    {ok,S} = get_statistics(Name),
+    Acc = get_counters_(Name, 2, record_info(fields, rtnl_link_stats),S,[]),
+    lists:reverse(Acc).
+
+select_counters_([Name|As],Fi,Var,Acc) ->
+    {ok,S} = get_statistics(Name),
+    Value = element(Fi,S),
+    select_counters_(As,Fi,Var,[{Name++"."++Var,Value}|Acc]);
+select_counters_([], _Fi, _Var, Acc) ->
+    lists:reverse(Acc).
+
+get_all_counters_([Name|As],Acc) ->
+    {ok,S} = get_statistics(Name),
+    Acc1 = get_counters_(Name,2,record_info(fields, rtnl_link_stats),S,Acc),
+    get_all_counters_(As,Acc1);
+get_all_counters_([], Acc) ->
+    lists:reverse(Acc).
+
+get_counters_(Name, I, [F|Fs], S, Acc) ->
+    Var = atom_to_list(F),
+    get_counters_(Name, I+1, Fs, S, [{Name++"."++Var, element(I,S)} |Acc]);
+get_counters_(_Name, _I, [], _S, Acc) ->
+    Acc.
+
+%%
+%% Find first Key in Position Pos in the List 
+%% return the position in the list or 0 if not found
+index(Value, List) ->
+    index_(1, Value, List).
+
+index_(I,Value,[Value|_List]) ->  I;
+index_(I,Value,[_|List]) -> index_(I+1,Value,List);
+index_(_I,_Value,[]) -> 0.
+
+
+get_statistics(Interface) ->
     netlink:start(),
     {ok,Ref} = netlink:subscribe(Interface),
     netlink:invalidate(Interface, [stats,stats64]),
